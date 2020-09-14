537ec12d02cfb85b9b469b42a2896469e1a4fd5d
Flávio José <flavioflavioj@hotmail.com>
2020-09-14 20:29:46 -0300

    Add files via upload
    nova atividade que envolve contagem.

---
+++ PrintNomeContagem.py
@@ -0,0 +1,9 @@
+print('Flávio José de Almeida Filho')
+print('-------------------------------------------------')
+
+contagem = 10
+print('Aquele foguete vai voar em 10 segundos, conta aí:')
+while contagem >= 0:
+    print(contagem)
+    contagem = contagem - 1
+print('Viu que massa?')
